3af7e1b689df227d258540b3b90da588069c7549
shubham <shubhamkarwariya245@gmail.com>
2025-01-08 10:10:58 +0530

    Add GenAI - Task 2: Shubham

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-import ollama from "ollama";
+//Task_1
 
-async function runChat() {
+// import ollama from "ollama";
+
+// async function runChat() {
+//   try {
+//     const response = await ollama.chat({
+//       model: "llama3.2:latest",
+//       messages: [{ role: 'user', content: "Generate a resignation letter" }]
+//     });
+
+//     console.log("Chatbot Response:", response.message.content);
+//   } catch (error) {
+//     console.error("Error occurred:", error.message);
+//   }
+// }
+
+// runChat();
+
+//----------------------------------------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------------------------------//
+
+//Task_2
+
+import ollama from "ollama";
+import fs from "fs";
+
+let q= fs.readFileSync("./q.txt", "utf-8");
+console.log(q)
+
+askQuestion()
+async function askQuestion() {
   try {
     const response = await ollama.chat({
       model: "llama3.2:latest",
-      messages: [{ role: 'user', content: "Generate a resignation letter" }]
+      messages: [{ role: 'user', content: q }]
     });
 
-    console.log("Chatbot Response:", response.message.content);
+    fs.writeFileSync("./a.txt", response.message.content);
+
   } catch (error) {
     console.error("Error occurred:", error.message);
   }
 }
-
-runChat();
